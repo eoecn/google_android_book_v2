bc0188574101bdfd40d9179888c52b2016313c4d
Vincent 4J <vincent4j@gmail.com>
2013-07-09 13:08:10 +0800

    replace tab with whitespaces in 19th

--- 19/BookSearcher/src/com/eoeandroid/booksearcher/BookInfo.java
+++ 19/BookSearcher/src/com/eoeandroid/booksearcher/BookInfo.java
@@ -6,102 +6,102 @@ import android.os.Parcelable;
 
 public class BookInfo implements Parcelable {
 
-	private String mTitle = ""; // 书名
-	private Bitmap mCover; // 封面
-	private String mAuthor = ""; // 作者
-	private String mPublisher = ""; // 出版社
-	private String mPublishDate = ""; // 出版时间
-	private String mISBN = ""; // ISBN
-	private String mSummary = ""; // 内容介绍
-	
-	public static final Parcelable.Creator<BookInfo> CREATOR = new Creator<BookInfo>() {
+    private String mTitle = ""; // 书名
+    private Bitmap mCover; // 封面
+    private String mAuthor = ""; // 作者
+    private String mPublisher = ""; // 出版社
+    private String mPublishDate = ""; // 出版时间
+    private String mISBN = ""; // ISBN
+    private String mSummary = ""; // 内容介绍
 
-		public BookInfo createFromParcel(Parcel source) {
-			BookInfo bookInfo = new BookInfo();
-			bookInfo.mTitle = source.readString();
-			bookInfo.mCover = source.readParcelable(Bitmap.class.getClassLoader());
-			bookInfo.mAuthor = source.readString();
-			bookInfo.mPublisher = source.readString();
-			bookInfo.mPublishDate = source.readString();
-			bookInfo.mISBN = source.readString();
-			bookInfo.mSummary = source.readString();
-			return bookInfo;
-		}
+    public static final Parcelable.Creator<BookInfo> CREATOR = new Creator<BookInfo>() {
 
-		public BookInfo[] newArray(int size) {
-			return new BookInfo[size];
-		}
-		
-	};
-	
-	public int describeContents() {
-		return 0;
-	}
+        public BookInfo createFromParcel(Parcel source) {
+            BookInfo bookInfo = new BookInfo();
+            bookInfo.mTitle = source.readString();
+            bookInfo.mCover = source.readParcelable(Bitmap.class.getClassLoader());
+            bookInfo.mAuthor = source.readString();
+            bookInfo.mPublisher = source.readString();
+            bookInfo.mPublishDate = source.readString();
+            bookInfo.mISBN = source.readString();
+            bookInfo.mSummary = source.readString();
+            return bookInfo;
+        }
 
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(mTitle);
-		dest.writeParcelable(mCover, flags);
-		dest.writeString(mAuthor);
-		dest.writeString(mPublisher);
-		dest.writeString(mPublishDate);
-		dest.writeString(mISBN);
-		dest.writeString(mSummary);
-	}
+        public BookInfo[] newArray(int size) {
+            return new BookInfo[size];
+        }
 
-	public String getTitle() {
-		return mTitle;
-	}
+    };
 
-	public void setTitle(String title) {
-		mTitle = title;
-	}
-	
-	public Bitmap getCover() {
-		return mCover;
-	}
+    public int describeContents() {
+        return 0;
+    }
 
-	public void setCover(Bitmap cover) {
-		mCover = cover;
-	}
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeParcelable(mCover, flags);
+        dest.writeString(mAuthor);
+        dest.writeString(mPublisher);
+        dest.writeString(mPublishDate);
+        dest.writeString(mISBN);
+        dest.writeString(mSummary);
+    }
 
-	public String getAuthor() {
-		return mAuthor;
-	}
-	
-	public void setAuthor(String author) {
-		mAuthor = author;
-	}
+    public String getTitle() {
+        return mTitle;
+    }
 
-	public String getPublisher() {
-		return mPublisher;
-	}
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 
-	public void setPublisher(String publisher) {
-		mPublisher = publisher;
-	}
-	
-	public String getPublishDate() {
-		return mPublishDate;
-	}
+    public Bitmap getCover() {
+        return mCover;
+    }
 
-	public void setPublishDate(String publishDate) {
-		mPublishDate = publishDate;
-	}
-	
-	public String getISBN() {
-		return mISBN;
-	}
+    public void setCover(Bitmap cover) {
+        mCover = cover;
+    }
 
-	public void setISBN(String isbn) {
-		mISBN = "ISBN: " + isbn;
-	}
+    public String getAuthor() {
+        return mAuthor;
+    }
 
-	public String getSummary() {
-		return mSummary;
-	}
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
 
-	public void setSummary(String summary) {
-		mSummary = summary;
-	}
-	
+    public String getPublisher() {
+        return mPublisher;
+    }
+
+    public void setPublisher(String publisher) {
+        mPublisher = publisher;
+    }
+
+    public String getPublishDate() {
+        return mPublishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        mPublishDate = publishDate;
+    }
+
+    public String getISBN() {
+        return mISBN;
+    }
+
+    public void setISBN(String isbn) {
+        mISBN = "ISBN: " + isbn;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
 }
--- 19/BookSearcher/src/com/eoeandroid/booksearcher/Utils.java
+++ 19/BookSearcher/src/com/eoeandroid/booksearcher/Utils.java
@@ -27,189 +27,189 @@ import android.graphics.BitmapFactory;
 import android.util.Log;
 
 public class Utils {
-	
-	public static final String TAG = "BookSearcher";
+    
+    public static final String TAG = "BookSearcher";
 
-	/**
-	 * 从豆瓣下载数据，并得到图书详情
-	 */
-	public static NetResponse download(String url) {
-		Log.v(TAG, "download from: " + url);
-		
-		NetResponse ret = downloadFromDouban(url);
-		
-		JSONObject message = null;
-		
-		try {
-			message = new JSONObject(String.valueOf(ret.getMessage()));
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		
-		switch (ret.getCode()) {
-		case BookAPI.RESPONSE_CODE_SUCCEED:
-			// 正常数据，返回BookInfo对象
-			ret.setMessage(parseBookInfo(message));
-			break;
-			
-		default:
-			// 异常数据，返回错误原因
-			int errorCode = parseErrorCode(message); 
-			ret.setCode(errorCode);
-			ret.setMessage(getErrorMessage(errorCode));
-			break;
-		}
-		
-		return ret;
-	}
-	
-	/**
-	 * 从豆瓣返回的错误消息中解析出错误码
-	 */
-	private static int parseErrorCode(JSONObject json) {
-		int ret = BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION;
-		
-		if (json == null) {
-			return ret;
-		}
-		
-		try {
-			ret = json.getInt(BookAPI.TAG_ERROR_CODE);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		
-		return ret;
-	}
-	
-	private static NetResponse downloadFromDouban(String url) {
-		NetResponse ret = new NetResponse(BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION, null);
-		
-		BufferedReader reader = null;
-		HttpClient client = null;
-		HttpResponse response = null;
-		
-		BasicHttpParams httpParams = new BasicHttpParams();
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-		schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-		ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
+    /**
+     * 从豆瓣下载数据，并得到图书详情
+     */
+    public static NetResponse download(String url) {
+        Log.v(TAG, "download from: " + url);
+        
+        NetResponse ret = downloadFromDouban(url);
+        
+        JSONObject message = null;
+        
+        try {
+            message = new JSONObject(String.valueOf(ret.getMessage()));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        switch (ret.getCode()) {
+        case BookAPI.RESPONSE_CODE_SUCCEED:
+            // 正常数据，返回BookInfo对象
+            ret.setMessage(parseBookInfo(message));
+            break;
 
-		client = new DefaultHttpClient(cm, httpParams);
+        default:
+            // 异常数据，返回错误原因
+            int errorCode = parseErrorCode(message); 
+            ret.setCode(errorCode);
+            ret.setMessage(getErrorMessage(errorCode));
+            break;
+        }
+        
+        return ret;
+    }
 
-		try {
-			response = client.execute(new HttpGet(url));
-			ret.setCode(response.getStatusLine().getStatusCode());
-			
-			StringBuffer sb = new StringBuffer();
-			String line;
-			reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			
-			while ((line = reader.readLine()) != null) {
-				sb.append(line);
-			}
-			
-			Log.v(TAG, "The content of Book: " + sb.toString());
-			ret.setMessage(sb.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (client != null) {
-				client.getConnectionManager().shutdown();
-			}
-			
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return ret;
-	}
+    /**
+     * 从豆瓣返回的错误消息中解析出错误码
+     */
+    private static int parseErrorCode(JSONObject json) {
+        int ret = BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION;
+        
+        if (json == null) {
+            return ret;
+        }
+        
+        try {
+            ret = json.getInt(BookAPI.TAG_ERROR_CODE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        return ret;
+    }
+    
+    private static NetResponse downloadFromDouban(String url) {
+        NetResponse ret = new NetResponse(BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION, null);
+        
+        BufferedReader reader = null;
+        HttpClient client = null;
+        HttpResponse response = null;
+        
+        BasicHttpParams httpParams = new BasicHttpParams();
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+        schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+        ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
 
-	/**
-	 * 将JSONArray对象解析成特定格式的字符串，例如：
-	 * 		["string0", "string1"] -> "string0 string1"  
-	 */
-	private static String parseJSONArray2String(JSONArray json, String split) {
-		if ((json == null) || (json.length() < 1)) {
-			return null;
-		}
-		
-		StringBuffer sb = new StringBuffer();
-		
-		for (int i = 0; i < json.length(); i++) {
-			try {
-				sb = sb.append(json.getString(i));
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			
-			sb = sb.append(split);
-		}
-		
-		sb.deleteCharAt(sb.length() - 1);
-		
-		Log.v(TAG, "parseJSONArray2String(" + json.toString() + "): " + sb.toString());
-		return sb.toString();
-	}
+        client = new DefaultHttpClient(cm, httpParams);
 
-	/**
-	 * 将从豆瓣下载所得的书籍JSON文件解析成图书对象
-	 */
-	private static BookInfo parseBookInfo(JSONObject json) {
-		if (json == null) {
-			return null;
-		}
+        try {
+            response = client.execute(new HttpGet(url));
+            ret.setCode(response.getStatusLine().getStatusCode());
+            
+            StringBuffer sb = new StringBuffer();
+            String line;
+            reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            
+            while ((line = reader.readLine()) != null) {
+            	sb.append(line);
+            }
+            
+            Log.v(TAG, "The content of Book: " + sb.toString());
+            ret.setMessage(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                client.getConnectionManager().shutdown();
+            }
+            
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        return ret;
+    }
 
-		BookInfo bookInfo = null;
-		
-		try {
-			bookInfo = new BookInfo();
-			
-			bookInfo.setTitle(json.getString(BookAPI.TAG_TITLE));
-			bookInfo.setCover(downloadBitmap((json.getString(BookAPI.TAG_COVER))));
-			bookInfo.setAuthor(parseJSONArray2String(json.getJSONArray(BookAPI.TAG_AUTHOR), " "));
-			bookInfo.setPublisher(json.getString(BookAPI.TAG_PUBLISHER));
-			bookInfo.setPublishDate(json.getString(BookAPI.TAG_PUBLISH_DATE));
-			bookInfo.setISBN(json.getString(BookAPI.TAG_ISBN));
-			bookInfo.setSummary(json.getString(BookAPI.TAG_SUMMARY).replace("\n", "\n\n"));
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
+    /**
+     * 将JSONArray对象解析成特定格式的字符串，例如：
+     *      ["string0", "string1"] -> "string0 string1"  
+     */
+    private static String parseJSONArray2String(JSONArray json, String split) {
+        if ((json == null) || (json.length() < 1)) {
+            return null;
+        }
+        
+        StringBuffer sb = new StringBuffer();
+        
+        for (int i = 0; i < json.length(); i++) {
+            try {
+            	sb = sb.append(json.getString(i));
+            } catch (JSONException e) {
+            	e.printStackTrace();
+            }
+            
+            sb = sb.append(split);
+        }
+        
+        sb.deleteCharAt(sb.length() - 1);
+        
+        Log.v(TAG, "parseJSONArray2String(" + json.toString() + "): " + sb.toString());
+        return sb.toString();
+    }
 
-		return bookInfo;
-	}
-	
-	/**
-	 * 根据错误码找到对应错误详情字符串的编号
-	 */
-	private static int getErrorMessage(int errorCode) {
-		int ret = R.string.error_message_default;
-		
-		switch (errorCode) {
-		case BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION:
-			ret = R.string.error_message_net_exception;
-			break;
-			
-		case BookAPI.RESPONSE_CODE_ERROR_BOOK_NOT_FOUND:
-			ret = R.string.error_message_book_not_found;
-			break;
+    /**
+     * 将从豆瓣下载所得的书籍JSON文件解析成图书对象
+     */
+    private static BookInfo parseBookInfo(JSONObject json) {
+        if (json == null) {
+        	return null;
+        }
+        
+        BookInfo bookInfo = null;
+        
+        try {
+            bookInfo = new BookInfo();
+            
+            bookInfo.setTitle(json.getString(BookAPI.TAG_TITLE));
+            bookInfo.setCover(downloadBitmap((json.getString(BookAPI.TAG_COVER))));
+            bookInfo.setAuthor(parseJSONArray2String(json.getJSONArray(BookAPI.TAG_AUTHOR), " "));
+            bookInfo.setPublisher(json.getString(BookAPI.TAG_PUBLISHER));
+            bookInfo.setPublishDate(json.getString(BookAPI.TAG_PUBLISH_DATE));
+            bookInfo.setISBN(json.getString(BookAPI.TAG_ISBN));
+            bookInfo.setSummary(json.getString(BookAPI.TAG_SUMMARY).replace("\n", "\n\n"));
+        } catch (JSONException e) {
+        	e.printStackTrace();
+        }
 
-		default:
-			break;
-		}
-		
-		return ret;
-	}
+        return bookInfo;
+    }
+    
+    /**
+     * 根据错误码找到对应错误详情字符串的编号
+     */
+    private static int getErrorMessage(int errorCode) {
+        int ret = R.string.error_message_default;
+        
+        switch (errorCode) {
+        case BookAPI.RESPONSE_CODE_ERROR_NET_EXCEPTION:
+            ret = R.string.error_message_net_exception;
+            break;
+            
+        case BookAPI.RESPONSE_CODE_ERROR_BOOK_NOT_FOUND:
+            ret = R.string.error_message_book_not_found;
+            break;
+            
+        default:
+            break;
+        }
+        
+        return ret;
+    }
 
-	/**
-	 * 通过URL下载封面图片
-	 */
-	private static Bitmap downloadBitmap(String url) {
+    /**
+     * 通过URL下载封面图片
+     */
+    private static Bitmap downloadBitmap(String url) {
         HttpURLConnection conn = null;
         InputStream is = null;
         BufferedInputStream bis = null;
